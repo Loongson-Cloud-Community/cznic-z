497ec2c82575e7dff2750c3eaf647fc24dd3df4c
Jan Mercl <0xjnml@gmail.com>
2023-08-28 23:43:59 +0200

    v2: move docs to doc.go

---
+++ v2/doc.go
@@ -0,0 +1,24 @@
+// Copyright 2023 The Zlib-Go Authors. All rights reserved.
+// Use of the source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate go run generator.go
+
+// Package z is a ccgo/v4 compatible zlib library.
+//
+// # Installation
+//
+//	$ go get modernc.org/z/v2
+//
+// # Linking using ccgo
+//
+//	$ ccgo -v4 foo.c -lmodernc.org/z/v2
+//
+// # Documentation
+//
+// Available at https://godoc.org/modernc.org/z/v2
+//
+// # Builders
+//
+// Available at https://modern-c.appspot.com/-/builder/?importpath=modernc.org%2fz%2fv2
+package z // import "modernc.org/z/v2"
